960ecef0e09f302f4f681f5d2ccdc47e497a4f9a
Yujinndev <xyujinwho@gmail.com>
2024-02-03 22:57:38 +0800

    feat: add status on items

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ import TodoItem from "./TodoItem";
 function App() {
   const [newTodo, setNewTodo] = useState("");
   const [newIndex, setNewIndex] = useState("");
-  const [lists, setLists] = useState([{ id: 0, name: "Current todo" }]);
+  const [lists, setLists] = useState([
+    { id: 0, name: "Create Pseudocode", status: "Done" },
+    { id: 1, name: "Translate into Flowchart", status: "Ongoing" },
+  ]);
 
   const handleCreate = () => {
     if (newTodo === "") return;
@@ -19,7 +22,7 @@ function App() {
       isItemExisting.name = newTodo;
     } else {
       const maxId = Math.max(...lists.map((todo) => todo.id), 0);
-      setLists([...lists, { name: newTodo, id: maxId + 1 }]);
+      setLists([...lists, { name: newTodo, id: maxId + 1, status: "Ongoing" }]);
     }
 
     setNewTodo("");
--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,9 +1,17 @@
 function TodoItem({ details, onDelete, onEdit }) {
   return (
-    <div className="todo-item">
-      <li>
-        <em>{details.name}</em>
-      </li>
+    <div
+      className={`todo-item + ${
+        details.status === "Done" ? "done" : "ongoing"
+      }`}
+    >
+      <div className="badge">
+        <i>{details.status}</i>
+      </div>
+
+      <div>
+        <h3 className="details-text">{details.name}</h3>
+      </div>
 
       <div style={{ display: "flex", gap: 2 }}>
         <button className="btn" type="button" onClick={onDelete}>
